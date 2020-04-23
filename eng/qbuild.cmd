0faf5ec21f928e8805f67afec70f491cc806c35f
Chtoucas <chtoucas@narvalo.org>
2020-04-23 18:21:08 +0200

    Improve build scripts.

--- eng/qbuild.cmd
+++ eng/qbuild.cmd
@@ -1,13 +1,26 @@
 :: Beware, will crash if the packages were not restored before.
+::
+:: Examples:
+:: > qbuild /p:Property=Value .\src\Abc.Maybe\
+::
+:: Standard settings:
+:: > qbuild /p:SignAssembly=true
+:: > qbuild /p:CheckForOverflowUnderflow=true
+:: > qbuild /p:DebugType=embedded
+:: > qbuild /p:GenerateDocumentationFile=true   --> always included but can be overriden
+::                                              (to ensure that the XML comments are well-formed)
+::
+:: Local settings:
+:: > qbuild /p:DisplaySettings=true
+:: > qbuild /p:Retail=true
+:: > qbuild /p:PatchEquality=true
 
 @echo off
 @setlocal
 
 @pushd %~dp0\..
 
-@call dotnet build -c Release --no-restore %* ^
-    /p:GenerateDocumentationFile=true ^
-    /p:SignAssembly=true
+@call dotnet build -c Release --no-restore /p:GenerateDocumentationFile=true %*
 
 @popd
 
