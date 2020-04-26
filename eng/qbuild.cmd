94c95f9eef181923f2773506bc34dc71d213d3f9
Chtoucas <policromia@gmail.com>
2020-04-26 09:36:23 +0200

    Developer notes.

--- eng/qbuild.cmd
+++ eng/qbuild.cmd
@@ -26,6 +26,8 @@
 :: Remarks:
 :: - GenerateDocumentationFile can be overriden
 :: - TargetFrameworks can NOT be overriden
+::   BUT one can use TargetFramework (no "s") to target a specific framework
+::   Useful when we want to build projects like Abc.Tests, play or perf.
 @call dotnet build ^
   /p:GenerateDocumentationFile=true %* ^
   /p:TargetFrameworks=\"net461;netstandard2.0;netstandard2.1;netcoreapp3.1\"
