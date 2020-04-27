3a8c497216a79948e217c14f266e9dedc97d0929
Chtoucas <chtoucas@narvalo.org>
2020-04-27 12:11:52 +0200

    Fix warning NU1701.

--- eng/qbuild.cmd
+++ eng/qbuild.cmd
@@ -1,19 +1,33 @@
 :: Build the solution for all supported frameworks.
-:: Ensure that the XML comments are well-formed too (GenerateDocumentationFile)
-:: and that InternalsVisibleTo works (SignAssembly).
+:: Ensure that the XML comments are well-formed too (GenerateDocumentationFile).
+::
+:: Remarks:
+:: - GenerateDocumentationFile can be overriden
+:: - TargetFrameworks can NOT be overriden
+::   BUT one can use TargetFramework (no "s") to target a specific framework
+::
+:: When we want to build exe projects like Abc.Tests, play or perf for a
+:: specific platform, specify the project path, eg .\src\Abc.Tests,
+:: - Without TargetFramework, build for MaxApiPlatform (see Directory.Build.props).
+:: - With    TargetFramework, build for TargetFramework.
+::
+:: The default behaviour is to build for all supported targets, except that
+:: exe projects using a specific "TargetFramework", they are only built for it
+:: (see above). To build **all** projects (exe projs included) for **all**
+:: supported targets, use
+::   /p:TargetFramework=
 ::
 :: Examples:
 :: > qbuild -c Release                          --> default = Debug
 :: > qbuild /p:Property=Value
 :: > qbuild .\src\Abc.Maybe\
 ::
-:: Standard settings:
-:: > qbuild /p:SignAssembly=true                --> always included but can be overriden
+:: Standard settings (default = opposite value):
+:: > qbuild /p:SignAssembly=true
 :: > qbuild /p:CheckForOverflowUnderflow=false
-:: > qbuild /p:DebugType=embedded
 :: > qbuild /p:GenerateDocumentationFile=true   --> always included but can be overriden
 ::
-:: Solution-specific options: see Directory.Build.props
+:: Local options (see Directory.Build.props):
 :: > qbuild /p:DisplaySettings=true
 :: > qbuild /p:Retail=true
 :: > qbuild /p:PatchEquality=true
@@ -25,22 +39,13 @@
 @pushd %~dp0\..
 
 :: Package restore: we MUST always include netcoreapp3.1 (MaxApiPlatform).
+:: We do NOT include netstandard1.0 as it is only supported by Abc.Maybe.
 @set fmks="netcoreapp3.1;netstandard2.1;netstandard2.0;net461"
 
-@call dotnet restore %* /p:TargetFrameworks=\"%fmks%"
-
-:: Remarks:
-:: - GenerateDocumentationFile can be overriden
-:: - TargetFrameworks can NOT be overriden
-::   BUT one can use TargetFramework (no "s") to target a specific framework
-::   Useful when we want to build projects like Abc.Tests, play or perf for a
-::   specific platform.
-::   Targeting a single exe project, eg .\src\play
-::   - Without TargetFramework, build only MaxApiPlatform.
-::   - With    TargetFramework, build only TargetFramework.
-@call dotnet build --no-restore ^
+:: Do not invoke "dotnet restore" before, it will fail if we specify eg
+:: /p:TargetFramework= on the command-line.
+@call dotnet build ^
   /p:GenerateDocumentationFile=true ^
-  /p:SignAssembly=true ^
   %* ^
   /p:TargetFrameworks=\"%fmks%\"
 
