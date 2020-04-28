538607b70513b9bd6b83fac7b9827f5c33192bca
Chtoucas <chtoucas@narvalo.org>
2020-04-28 23:29:25 +0200

    Remove a bunch of projects.

--- src/integration/NET472/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// See LICENSE in the project root for license information.
-
-using System;
-using System.Runtime.InteropServices;
-
-[assembly: CLSCompliant(false)]
-[assembly: ComVisible(false)]
