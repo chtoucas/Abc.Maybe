6380ae420fca6adc4f93fa7b09d091777657f3d0
Chtoucas <policromia@gmail.com>
2020-04-28 08:08:20 +0200

    Clean up project files.

--- src/integration/NET45/Properties/AssemblyInfo.cs
+++ src/integration/NET45/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("44085b8a-0631-458a-8301-e7bb0b9c9594")]
