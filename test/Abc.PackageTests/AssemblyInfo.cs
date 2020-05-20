7c084ce7cf63e0c8ad2e9d4e2dea8a367288e7ff
Chtoucas <chtoucas@narvalo.org>
2020-05-20 18:02:08 +0200

    Let MSBuild generate the assembly info.

--- test/Abc.PackageTests/AssemblyInfo.cs
+++ test/Abc.PackageTests/AssemblyInfo.cs
@@ -1,22 +1,7 @@
 ﻿// See LICENSE in the project root for license information.
 
 using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
-
-[assembly: AssemblyTitle("Abc.PackageTests")]
-[assembly: AssemblyDescription("Test library for the package Abc.Maybe.")]
-[assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyCompany("Narvalo.Org")]
-[assembly: AssemblyProduct("Abécédaire Library")]
-[assembly: AssemblyCopyright("Copyright (c) 2019 Narvalo.Org")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-DUMMY")]
