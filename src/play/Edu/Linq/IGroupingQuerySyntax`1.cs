920ce5fc745597d3553a4509a852868fa80f58e8
Chtoucas <chtoucas@narvalo.org>
2020-05-24 17:25:43 +0200

    Remove project play.

--- src/play/Edu/Linq/IGroupingQuerySyntax`1.cs
+++
@@ -1,9 +0,0 @@
-﻿// See LICENSE in the project root for license information.
-
-namespace Abc.Edu.Linq
-{
-    public interface IGroupingQuerySyntax<TKey, T> : IQuerySyntax<T>
-    {
-        TKey Key { get; }
-    }
-}
