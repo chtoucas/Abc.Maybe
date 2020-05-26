837aee6d489e2432e9afdeaedd60e8a0c5630f05
Chtoucas <policromia@gmail.com>
2020-05-26 10:37:40 +0200

    Create test.ps1

---
+++ eng/test.ps1
@@ -0,0 +1,151 @@
+# See LICENSE in the project root for license information.
+
+################################################################################
+#region Preamble.
+
+<#
+.SYNOPSIS
+Run the test suite for all supported platforms.
+
+.PARAMETER Configuration
+The configuration to test the project/solution for. Default = "Debug".
+
+.PARAMETER Runtime
+The runtime to test the project/solution for.
+
+.PARAMETER Platform
+The single platform to test the project/solution for.
+
+.PARAMETER ListPlatforms
+Print the list of supported platforms, then exit.
+
+.PARAMETER NoRestore
+Do not restore the project/solution.
+
+#>
+[CmdletBinding(PositionalBinding = $false)]
+param(
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("Debug", "Release")]
+    [Alias("c")] [string] $Configuration,
+
+    [Parameter(Mandatory = $false)]
+                 [string] $Runtime,
+
+    [Parameter(Mandatory = $false)]
+    [Alias("f")] [string] $Platform,
+                 [switch] $ListPlatforms,
+
+                 [switch] $NoRestore,
+    [Alias("h")] [switch] $Help,
+
+    [Parameter(Mandatory=$false, ValueFromRemainingArguments = $true)]
+                 [string[]] $Properties
+)
+
+. (Join-Path $PSScriptRoot "abc.ps1")
+
+# ------------------------------------------------------------------------------
+
+const TEST_PROJECT_NAME "Abc.Tests"
+const TEST_PROJECT (Join-Path $SRC_DIR $TEST_PROJECT_NAME -Resolve)
+
+#endregion
+################################################################################
+#region Helpers
+
+function Print-Help {
+    say @"
+
+Run the test suite for all supported platforms.
+
+Usage: reset.ps1 [arguments]
+  -c|-Configuration     the configuration to test the project/solution for.
+     -Runtime           the runtime to test the project/solution for.
+
+  -f|-Platform          the platform to test the project/solution for.
+     -ListPlatforms     print the list of supported platforms, then exit.
+
+     -NoRestore         do not restore the project/solution.
+  -h|-Help              print this help and exit.
+
+Arguments starting with '/p:' are passed through to dotnet.exe.
+> test.ps1 /p:Retail=true
+> test.ps1 /p:HideInternals=true
+> test.ps1 /p:PatchEquality=true
+> test.ps1 /p:PrintSettings=true
+
+Examples.
+> test.ps1
+> test.ps1 /p:Retail=true
+> test.ps1 -c Release
+
+Looking for more help?
+> Get-Help -Detailed test.ps1
+
+"@
+}
+
+#endregion
+################################################################################
+#region Main.
+
+if ($Help) { Print-Help ; exit }
+
+Hello "this is the test script.`n"
+
+try {
+    ___BEGIN___
+
+    $platforms = Get-TestPlatforms
+    $minClassic, $maxClassic, $minCore, $maxCore  = Get-SupportedPlatforms
+    $allPlatforms = $maxCore + $maxClassic
+
+    if ($ListPlatforms) {
+        say ("Default platform set:`n- {0}" -f ($platforms -join "`n- "))
+        say ("`nSupported platforms (option -Platform):`n- {0}" -f ($allPlatforms -join "`n- "))
+        exit
+    }
+
+    $args = @()
+    if ($Configuration) { $args += "-c:$Configuration" }
+    if ($Runtime)       { $args += "--runtime:$runtime" }
+    if ($NoRestore)     { $args += "--no-restore" }
+
+    # TODO: does not work.
+    # All platforms: replace "TargetFramework" by "TargetFrameworks" in csproj?
+    #   See build.ps1 too if we do so.
+    # Fails with netcoreapp2.0???
+    #   Unable to find "bin\Debug\netcoreapp2.0\testhost.dll".
+    # Property IsTestProject?
+    if ($Platform)  {
+        if ($Platform -notin $allPlatforms) {
+            die "The specified platform is not supported: ""$Platform""."
+        }
+
+        $args += "/p:TargetFrameworks=$Platform", "/p:TargetFramework=$Platform"
+        #$args += "-f:$Platform"
+    }
+    else {
+        $args += '/p:TargetFrameworks=\"' + ($platforms -join ";") + '\"'
+    }
+
+    foreach ($arg in $Properties) {
+        if ($arg.StartsWith("/p:", "InvariantCultureIgnoreCase")) {
+            $args += $arg
+        }
+    }
+
+    # Do not invoke "dotnet restore" before, it will fail w/ -Platform.
+    & dotnet test $TEST_PROJECT $args
+        || die "Test task failed."
+}
+catch {
+    ___CATCH___
+}
+finally {
+    ___END___
+}
+
+#endregion
+################################################################################
