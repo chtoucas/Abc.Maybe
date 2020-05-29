fdc734ba1ded1490984ee2bdfdc9dfe2f76b1492
Chtoucas <chtoucas@narvalo.org>
2020-05-29 23:34:27 +0200

    Extract the timestamp stored within FileVersion.

--- eng/asm.ps1
+++ eng/asm.ps1
@@ -6,27 +6,56 @@
 param(
     [Parameter(Mandatory = $false, Position = 0)]
     [ValidateNotNullOrEmpty()]
-    [string] $path
+    [string] $Path,
+
+    [switch] $NoTimestamp
 )
 
 . (Join-Path $PSScriptRoot "abc.ps1")
 
+# ------------------------------------------------------------------------------
+
+function Get-Timestamp {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $fileVersion
+    )
+
+    $parts = $fileVersion.Split(".")
+    [int] $halfdays = $parts[2]
+    [int] $seconds  = $parts[3]
+    $orig = New-Object DateTime(2020, 1, 1, 0, 0, 0, [System.DateTimeKind]::Utc)
+
+    $orig.AddSeconds(43200 * $halfdays + $seconds)
+}
+
+# ------------------------------------------------------------------------------
+
+Hello "this is the script to extract informations from an assembly."
+
 try {
     ___BEGIN___
 
-    if (-not (Test-Path $path)) {
+    if (-not (Test-Path $Path)) {
         die "The file does NOT exist."
     }
-    if (-not [System.IO.Path]::IsPathRooted($path)) {
-        $path = Join-Path (Get-Location) $path -Resolve
+    if (-not [System.IO.Path]::IsPathRooted($Path)) {
+        $Path = Join-Path (Get-Location) $Path -Resolve
     }
 
-    $asm = [System.Reflection.Assembly]::LoadFile($path)
+    $asm = [System.Reflection.Assembly]::LoadFile($Path)
     # System.Diagnostics.FileVersionInfo
-    $fileInfo = Get-Item $path | % VersionInfo
+    $fileInfo = Get-Item $Path | % VersionInfo
 
     SAY-LOUDLY "`nAssembly's Full Name."
     say $asm.FullName
+
+    if (-not $NoTimestamp) {
+        SAY-LOUDLY "`nAssembly's Timestamp."
+        say (Get-Timestamp $fileInfo.FileVersion).ToString("r")
+    }
 
     SAY-LOUDLY "`nAssembly's Version Attributes."
     say ("AssemblyVersion      = {0}" -f $asm.GetName().Version)
@@ -42,3 +71,5 @@ catch {
 finally {
     ___END___
 }
+
+################################################################################
