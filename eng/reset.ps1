bbf40320d39fb9d0459f665e3939dfed0934f191
Chtoucas <policromia@gmail.com>
2020-05-31 09:11:04 +0200

    reset.ps1: new option to remove more artifacts.

--- eng/reset.ps1
+++ eng/reset.ps1
@@ -1,8 +1,14 @@
 # See LICENSE in the project root for license information.
 
+################################################################################
+#region Preamble.
+
 <#
 .SYNOPSIS
 Reset the repository.
+
+.PARAMETER Extended
+Delete even more untracked files.
 
 .PARAMETER Restore
 Restore NuGet packages and tools thereafter.
@@ -12,6 +18,7 @@ Do not ask for confirmation.
 #>
 [CmdletBinding()]
 param(
+    [Alias("x")] [switch] $Extended,
                  [switch] $Restore,
     [Alias("y")] [switch] $Yes,
     [Alias("h")] [switch] $Help
@@ -19,7 +26,46 @@ param(
 
 . (Join-Path $PSScriptRoot "abc.ps1")
 
+#endregion
+################################################################################
+#region Tasks.
+
+function Delete-Artifacts {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $dirname,
+
+        [Alias("y")] [switch] $yes
+    )
+
+    if ($yes) { say "`nDeleting artifacts directory ""$dirname""." }
+
+    if ($yes -or (yesno "`nDelete artifacts directory ""$dirname""?")) {
+        Remove-Dir (Join-Path $ARTIFACTS_DIR $dirname)
+        say-softly "Directory ""$dirname"" was deleted."
+    }
+}
+
 # ------------------------------------------------------------------------------
+
+function Reset-EngTree {
+    [CmdletBinding()]
+    param(
+        [Alias("y")] [switch] $yes
+    )
+
+    if ($yes) { say "`nResetting engineering tree." }
+
+    if ($yes -or (yesno "`nReset the engineering tree?")) {
+        Remove-BinAndObj $ENG_DIR
+        say-softly "The engineering tree was reset."
+    }
+}
+
+#endregion
+################################################################################
 
 if ($Help) {
     say @"
@@ -36,22 +82,25 @@ Usage: reset.ps1 [arguments]
     exit
 }
 
-Hello "this is the reset script."
+Hello "this is the cleanup script."
 
 try {
     ___BEGIN___
 
-    # Folders that we do NOT reset:
-    # 1) __\coverlet\
-    # 2) __\opencover\
-    # 3) __\tools\
-    # 4) eng\NETFxTools\
-
+    Reset-EngTree         -Yes:$Yes
     Reset-SourceTree      -Yes:$Yes
     Reset-TestTree        -Yes:$Yes
     Reset-PackageOutDir   -Yes:$Yes
     Reset-PackageCIOutDir -Yes:$Yes
     Reset-LocalNuGet      -Yes:$Yes
+
+    if ($Extended) {
+        # TODO: reset folder "__\tools\".
+
+        Delete-Artifacts "benchmarks" -Yes:$Yes
+        Delete-Artifacts "coverlet"   -Yes:$Yes
+        Delete-Artifacts "opencover"  -Yes:$Yes
+    }
 
     if ($Restore) {
         SAY-LOUDLY "`nRestoring dependencies, please wait..."
