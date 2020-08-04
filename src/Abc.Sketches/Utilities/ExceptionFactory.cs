b741b391bf8c52b90b78fb9dd1fe13103ca233dd
Chtoucas <policromia@gmail.com>
2020-08-04 10:59:50 +0200

    Update ExceptionFactory.cs

--- src/Abc.Sketches/Utilities/ExceptionFactory.cs
+++ src/Abc.Sketches/Utilities/ExceptionFactory.cs
@@ -13,15 +13,10 @@ namespace Abc.Utilities
     /// </summary>
     internal static class ExceptionFactory
     {
-        public static readonly InvalidOperationException EmptySequence
-            = new InvalidOperationException("The sequence was empty.");
+        public static InvalidOperationException EmptySequence =>
+            new InvalidOperationException("The sequence was empty.");
 
-        public static InvalidOperationException ControlFlow
-            => new InvalidOperationException(
-                "The flow of execution just reached a section of the code that should have been unreachable."
-                + $"{Environment.NewLine}Most certainly signals a coding error. Please report.");
-
-        public static readonly ArgumentException MaybeComparer_InvalidType
-            = new ArgumentException("Type of argument is not compatible with MaybeComparer<T>.");
+        public static ArgumentException MaybeComparer_InvalidType =>
+            new ArgumentException("Type of argument is not compatible with MaybeComparer<T>.");
     }
 }
